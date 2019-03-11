28c4fe2ec8e96d0de96fb0b69011d268b280f083
SNWarbard <bilel.lahbib01@gmail.com>
2019-03-11 14:43:29 +0000

    Body

---
+++ public/js/HomeJs.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin: 30,
+        loop: true,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplayHoverPause: true,
+        nav: true,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    });
+  });
