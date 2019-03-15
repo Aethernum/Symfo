35091488098b49c2c65a7056aec177872cd133d8
Aethernum1 <martinsko@sfr.fr>
2019-03-15 13:33:41 +0000

    Evenemetn CRUD

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,35 +2,97 @@
 
 namespace App\Controller;
 
-use App\Form\EvenementType;
 use App\Entity\Evenement;
-use Symfony\Component\HttpFoundation\Request;
+use App\Form\Evenement1Type;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
+/**
+ * @Route("/evenement")
+ */
 class EvenementController extends AbstractController
 {
     /**
-     * @Route("/evenements", name="evenement")
+     * @Route("/", name="evenement_index", methods={"GET"})
      */
-    public function index(Request $request, EntityManagerInterface $entityManager)
+    public function index(EvenementRepository $evenementRepository): Response
     {
+        return $this->render('evenement/index.html.twig', [
+            'evenements' => $evenementRepository->findAll(),
+            'name' => 'Evenement'
+        ]);
+    }
 
+    /**
+     * @Route("/new", name="evenement_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
         $evenement = new Evenement();
-
-        $form = $this->createForm(EvenementType::class, $evenement);
+        $form = $this->createForm(Evenement1Type::class, $evenement);
         $form->handleRequest($request);
-        if ( $form->isSubmitted() && $form->isValid()) {
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($evenement);
             $entityManager->flush();
-            return $this->redirectToRoute("evenement");
+
+            return $this->redirectToRoute('evenement_index');
         }
 
-        return $this->render('evenement/index.html.twig', [
-            'controller_name' => 'EvenementController',
-            'form'=>$form->createView(),
-            'name' => 'Evenement',
+        return $this->render('evenement/new.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+            'name' => 'Ajouter un evenement',
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="evenement_show", methods={"GET"})
+     */
+    public function show(Evenement $evenement): Response
+    {
+        return $this->render('evenement/show.html.twig', [
+            'evenement' => $evenement,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="evenement_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Evenement $evenement): Response
+    {
+        $form = $this->createForm(Evenement1Type::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('evenement_index', [
+                'id' => $evenement->getId(),
+            ]);
+        }
+
+        return $this->render('evenement/edit.html.twig', [
+            'evenement' => $evenement,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="evenement_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Evenement $evenement): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$evenement->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($evenement);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('evenement_index');
     }
 }
